2347eca817b6e530ab9ea23406c82b20ae675811
Yrocky <983272765@qq.com>
2016-11-15 21:54:50 +0800

    Initial Commit

---
+++ HLLLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HLLLabel
+//
+//  Created by Rocky Young on 16/11/15.
+//  Copyright © 2016年 HLL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HLLLabel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HLLLabel
+//
+//  Created by Rocky Young on 16/11/15.
+//  Copyright © 2016年 HLL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
